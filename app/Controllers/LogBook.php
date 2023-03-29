711632618fe20a559f64183e8effe6f62337f09f
halimkun <ffaisalhalim@gmail.com>
2023-03-29 14:55:41 +0700

    update logbook structure

--- app/Controllers/LogBook.php
+++ app/Controllers/LogBook.php
@@ -19,12 +19,16 @@ class LogBook extends BaseController
             'id_siswa'      => $this->request->getPost('sid'),
             'id_tempat'     => $this->request->getPost('tid'),
             'id_pembimbing' => $this->request->getPost('pid'),
-            'tanggal'       => $this->request->getPost('tanggal'),
-            'jam_masuk'     => $this->request->getPost('jam_masuk'),
-            'jam_keluar'    => $this->request->getPost('jam_keluar'),
             'keterangan'    => $this->request->getPost('keterangan'),
-            'kegiatan'      => $this->request->getPost('kegiatan'),
+            'tanggal'       => $this->request->getPost('tanggal'),
         ];
+
+        // if keterangan is = hadir
+        if ($data['keterangan'] == 'hadir') {
+            $data['jam_masuk']  = $this->request->getPost('jam_masuk');
+            $data['jam_keluar'] = $this->request->getPost('jam_keluar');
+            $data['kegiatan']   = $this->request->getPost('kegiatan');
+        }
 
         // insert data and return json
         if ($this->logbooks->insert($data)) {
@@ -38,6 +42,30 @@ class LogBook extends BaseController
                 'success'   => false,
                 'status'    => 500,
                 'message'   => 'Data gagal disimpan',
+            ];
+        }
+
+        return $this->response->setJSON($response);
+    }
+
+    public function status_update()
+    {
+        $data = [
+            'id'            => $this->request->getPost('id'),
+            'status'        => $this->request->getPost('status'),
+        ];
+
+        if ($this->logbooks->update($data['id'], $data)) {
+            $response = [
+                'success'   => true,
+                'status'    => 200,
+                'message'   => 'Status berhasil diubah',
+            ];
+        } else {
+            $response = [
+                'success'   => false,
+                'status'    => 500,
+                'message'   => 'Status gagal diubah',
             ];
         }
 
