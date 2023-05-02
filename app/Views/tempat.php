471782b023a29e30f41fd07194ca92a551570dca
halimkun <ffaisalhalim@gmail.com>
2023-05-02 09:41:36 +0700

    helper function check availability

--- app/Views/tempat.php
+++ app/Views/tempat.php
@@ -46,7 +46,7 @@
         }
         ?>
         <div class="col-12 col-sm-6 col-md-4 col-xxl-3 mb-4 cardInstansi <?= $t->status ?>">
-            <div class="card mt-4" data-animation="<?= !empty(getApplicationSiswa(getSid(user_id()))) || !getSlotAvailable($t->id) == 0 ? 'true' : 'false' ?>">
+            <div class="card mt-4" data-animation="<?= initCheck(user_id(), $t->id) ? 'true' : 'false' ?>">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                     <a class="d-block blur-shadow-image">
                         <img src="<?= $foto ?>" alt="img-blur-shadow" height="200px" class="img-fluid shadow border-radius-lg">
@@ -54,9 +54,9 @@
                     <div class="colored-shadow" style="background-image: url(&quot;<?= $foto ?>&quot;);"></div>
                 </div>
                 <div class="card-body text-center">
-                    <div class="<?= !empty(getApplicationSiswa(getSid(user_id()))) || !getSlotAvailable($t->id) == 0 ? 'mt-n6' : 'mt-n4' ?> mx-auto">
-                        <?php if (!empty(getApplicationSiswa(getSid(user_id()))) || !getSlotAvailable($t->id) == 0) : ?>
-                            <button type="button" class="btn bg-gradient-primary btn-sm mb-0 me-2 btn-daftar" data-instansi="<?= $t->nama ?>" data-uid="<?= getSid(user_id()) ?>" data-tid="<?= $t->id ?>" <?= $t->status == 'tutup' || getSlotAvailable($t->id) == 0 ? 'disabled' : '' ?>>Daftar</button>
+                    <div class="<?= initCheck(user_id(), $t->id) ? 'mt-n6' : 'mt-n4' ?> mx-auto">
+                        <?php if (initCheck(user_id(), $t->id)) : ?>
+                            <button type="button" class="btn bg-gradient-primary btn-sm mb-0 me-2 btn-daftar" data-instansi="<?= $t->nama ?>" data-uid="<?= getSidByUid(user_id()) ?>" data-tid="<?= $t->id ?>" <?= $t->status == 'tutup' || getSlotAvailable($t->id) == 0 ? 'disabled' : '' ?>>Daftar</button>
                         <?php endif ?>
                     </div>
                     <h6 class="font-weight-normal mt-4"><?= $t->nama ?></h6>
