d390981df43ddc3ee9ec75def09b6e3a4bab5afb
FAISAL HALIM <ffaisalhalim@gmail.com>
2023-06-10 17:44:27 +0700

    add logo

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -10,6 +10,9 @@
 					<div class="card z-index-0 fadeIn3 fadeInBottom">
 						<div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
 							<div class="bg-gradient-dark shadow-dark border-radius-lg py-3 pe-1">
+								<div class="text-center">
+									<img src="/assets/logo.png" class="navbar-brand-img" style="width: 100px;" alt="main_logo">
+								</div>
 								<h4 class="text-white font-weight-bolder text-center mt-2 mb-0"><?= lang('Auth.loginTitle') ?></h4>
 								<div class="row mt-0">
 									<div class="col-12 text-center px-1">
